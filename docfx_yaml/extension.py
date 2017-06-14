de7aa55e17dc7db59b7dd9ec540b26dfbea04c6d
Yiding Tian <jaytin1218@hotmail.com>
2017-06-14 16:38:51 +0800

    explain 'see also' modify

--- docfx_yaml/extension.py
+++ docfx_yaml/extension.py
@@ -422,7 +422,7 @@ def build_finished(app, exception):
 
                     # Raise up seealso
                     if 'seealso' in obj['syntax'] and obj['syntax']['seealso']:
-                        obj['seealsoContent'] = obj['syntax'].pop('seealso')[10:]
+                        obj['seealsoContent'] = obj['syntax'].pop('seealso')[10:]  # 10=len('See also: ')
 
                     # Raise up example
                     if 'example' in obj['syntax'] and obj['syntax']['example']:
