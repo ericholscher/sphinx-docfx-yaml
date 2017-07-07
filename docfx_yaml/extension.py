d9bd91537f1ae9ebdf82f699331af6dc961ec59c
Yannan Wang <yannw@microsoft.com>
2017-07-07 11:35:23 +0800

    Fix Bug: Return type should be clickable

--- docfx_yaml/extension.py
+++ docfx_yaml/extension.py
@@ -553,7 +553,7 @@ def missing_reference(app, env, node, contnode):
             module = node['py:module']
 
         #Refactor reftarget to fullname if it is a short name
-        if reftype in [CLASS, REFFUNCTION, REFMETHOD] and module and not reftarget.startswith(module.split('.')[0]):
+        if reftype in [CLASS, REFFUNCTION, REFMETHOD] and module and len(reftarget.split('.')) == 1:
             if reftype in [CLASS, REFFUNCTION]:
                 fields = (module, reftarget)
             else:
