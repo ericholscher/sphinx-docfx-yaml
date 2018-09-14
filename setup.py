cd3f0452b69835c801ecfc630cd0fbc0ab449591
xavier dupré <xavier.dupre@gmail.com>
2018-09-14 15:16:01 +0200

    remove markdown outputter, upgrade version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ extra_setup = dict(
 install_requires=[
     'PyYAML',
     'wheel>=0.24.0',
-    'sphinx==1.5.5',
+    'sphinx>=1.5.5',
     'unidecode',
 ],
 setup_requires=['pytest-runner'],
@@ -13,7 +13,7 @@ tests_require=['pytest', 'mock'],
 
 setup(
     name='sphinx-docfx-yaml',
-    version='1.2.43',
+    version='1.2.5',
     author='Eric Holscher',
     author_email='eric@ericholscher.com',
     url='https://github.com/ericholscher/sphinx-docfx-yaml',
